ab7f594510453a12f4b783e2b3a9925b0e2dc926
Yasmin-Avraham <yasmin.avr96@gmail.com>
2026-02-15 19:57:41 +0200

    run build

--- src/main/java/com/taskmind/gateway/GatewayServiceApplication.java
+++ src/main/java/com/taskmind/gateway/GatewayServiceApplication.java
@@ -3,7 +3,7 @@ package com.taskmind.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // class for gateway
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
